baf6c6b24f6290d30278d3aaca2cb8c9538a625e
Van Brantley <van.brantley@gmail.com>
2023-08-31 16:26:05 -0400

    Test Vercel update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -92,7 +92,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
+            Discover and deploy boilerplate example Next.js&nbsp;projects. TEST ADDITION!
           </p>
         </a>
 
